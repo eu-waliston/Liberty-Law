97dab0298914b8b2882fa4fb6298aea1b9cb1df8
Waliston Euripedes <goneisgone@hotmail.com>
2022-11-09 19:27:25 -0300

    Liberty Law

---
+++ js/index.js
@@ -0,0 +1,68 @@
+// Seleção de elements 
+const menuBtn = document.querySelector("#menu");
+const ClosemenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+const slides = document.querySelectorAll('.banner');
+const dots = document.querySelectorAll('.dot');
+let slideIndex = 0;
+
+
+//Funcções
+function smoothScrol(e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+    
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    })
+
+   setTimeout( () =>{
+    if(menu.classList.contains("menu-active")) {
+        menu.classList.remove("menu-active");
+    }
+   }, 500)
+}
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+       slides[i].classList.remove("active")  
+       dots[i].classList.remove("active")           
+    }
+
+    slideIndex++;
+
+    if(slideIndex > slides.length) {
+        slideIndex  = 1;
+    }
+
+
+    slides[slideIndex - 1].classList.add("active");
+    dots[slideIndex - 1].classList.add("active");
+
+    setTimeout(showSlides, 3000)
+
+}
+
+
+//Eventos 
+[menuBtn, ClosemenuBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        menu.classList.toggle("menu-active")
+    })
+})
+
+allLinks.forEach((link) => {
+
+    link.addEventListener("click", smoothScrol)
+})
+
+//Inicialização
+showSlides();
